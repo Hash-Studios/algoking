20d83da2955bcb00cb1f053629d306b7b4f277b6
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-05-11 04:27:27 +0530

    Added some more desc

--- app/lib/desc/desc.dart
+++ app/lib/desc/desc.dart
@@ -44,18 +44,197 @@ A simple approach is to do linear search, i.e
 The time complexity of above algorithm is O(n).
 
 Linear search is rarely used practically because other search algorithms such as the binary search algorithm and hash tables allow significantly faster searching comparison to Linear search.""",
-    "Bubble_Sort": """""",
-    "Heap_Sort": """""",
-    "Insertion_Sort": """""",
-    "Merge_Sort": """""",
-    "Quick_Sort": """""",
-    "Selection_Sort": """""",
+    "Bubble_Sort":
+        """Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+    
+Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+
+Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+
+Auxiliary Space: O(1)
+
+Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
+
+Sorting In Place: Yes
+
+Stable: Yes
+
+Due to its simplicity, bubble sort is often used to introduce the concept of a sorting algorithm.
+In computer graphics it is popular for its capability to detect a very small error (like swap of just two elements) in almost-sorted arrays and fix it with just linear complexity (2n).""",
+    "Heap_Sort":
+        """Heap sort is a comparison based sorting technique based on Binary Heap data structure. It is similar to selection sort where we first find the maximum element and place the maximum element at the end. We repeat the same process for remaining element.
+
+Why array based representation for Binary Heap?
+Since a Binary Heap is a Complete Binary Tree, it can be easily represented as array and array based representation is space efficient. If the parent node is stored at index I, the left child can be calculated by 2 * I + 1 and right child by 2 * I + 2 (assuming the indexing starts at 0).
+
+Heap Sort Algorithm for sorting in increasing order:
+1. Build a max heap from the input data.
+2. At this point, the largest item is stored at the root of the heap. Replace it with the last item of the heap followed by reducing the size of heap by 1. Finally, heapify the root of tree.
+3. Repeat above steps while size of heap is greater than 1.
+""",
+    "Insertion_Sort":
+        """Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hands.
+
+Algorithm
+// Sort an arr[] of size n
+insertionSort(arr, n)
+Loop from i = 1 to n-1.
+……a) Pick element arr[i] and insert it into sorted sequence arr[0…i-1] 
+
+Time Complexity: O(n*2)
+
+Auxiliary Space: O(1)
+
+Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
+
+Algorithmic Paradigm: Incremental Approach
+
+Sorting In Place: Yes
+
+Stable: Yes
+
+Online: Yes
+
+Uses: Insertion sort is used when number of elements is small. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
+
+""",
+    "Merge_Sort":
+        """Like QuickSort, Merge Sort is a Divide and Conquer algorithm. It divides input array in two halves, calls itself for the two halves and then merges the two sorted halves. The merge() function is used for merging two halves. The merge(arr, l, m, r) is key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one. See following C implementation for details.
+    
+Time Complexity: Sorting arrays on different machines. Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+T(n) = 2T(n/2) + \Theta(n)
+The above recurrence can be solved either using Recurrence Tree method or Master method. It falls in case II of Master Method and solution of the recurrence is \Theta(nLogn).
+Time complexity of Merge Sort is \Theta(nLogn) in all 3 cases (worst, average and best) as merge sort always divides the array into two halves and take linear time to merge two halves.
+
+Auxiliary Space: O(n)
+
+Algorithmic Paradigm: Divide and Conquer
+
+Sorting In Place: No in a typical implementation
+
+Stable: Yes
+""",
+    "Quick_Sort": """
+
+Like Merge Sort, QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. There are many different versions of quickSort that pick pivot in different ways.
+
+    Always pick first element as pivot.
+    Always pick last element as pivot (implemented below)
+    Pick a random element as pivot.
+    Pick median as pivot.
+
+The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x. All this should be done in linear time.
+
+Is QuickSort stable?
+The default implementation is not stable. However any sorting algorithm can be made stable by considering indexes as comparison parameter.
+Is QuickSort In-place?
+As per the broad definition of in-place algorithm it qualifies as an in-place sorting algorithm as it uses extra space only for storing recursive function calls but not for manipulating the input.""",
+    "Selection_Sort":
+        """The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
+
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray. 
+
+Time Complexity: O(n2) as there are two nested loops.
+
+Auxiliary Space: O(1)
+The good thing about selection sort is it never makes more than O(n) swaps and can be useful when memory write is a costly operation.""",
     "Even_Odd": """""",
-    "Factorial": """""",
-    "Fibonacci": """""",
-    "Greatest_Common_Divisor": """""",
-    "Lowest_Common_Multiple": """""",
-    "Max_Pairwise_Product": """""",
+    "Factorial":
+        """Factorial of a non-negative integer, is multiplication of all integers smaller than or equal to n. For example factorial of 6 is 6*5*4*3*2*1 which is 720.
+
+How to compute factorial of 100 using a C/C++ program?
+Factorial of 100 has 158 digits. It is not possible to store these many digits even if we use long long int. 
+
+The following is detailed algorithm for finding factorial.
+
+factorial(n)
+1) Create an array ‘res[]’ of MAX size where MAX is number of maximum digits in output.
+2) Initialize value stored in ‘res[]’ as 1 and initialize ‘res_size’ (size of ‘res[]’) as 1.
+3) Do following for all numbers from x = 2 to n.
+……a) Multiply x with res[] and update res[] and res_size to store the multiplication result.
+
+How to multiply a number ‘x’ with the number stored in res[]?
+The idea is to use simple school mathematics. We one by one multiply x with every digit of res[]. The important point to note here is digits are multiplied from rightmost digit to leftmost digit. If we store digits in same order in res[], then it becomes difficult to update res[] without extra space. That is why res[] is maintained in reverse way, i.e., digits from right to left are stored.
+
+multiply(res[], x)
+1) Initialize carry as 0.
+2) Do following for i = 0 to res_size – 1
+….a) Find value of res[i] * x + carry. Let this value be prod.
+….b) Update res[i] by storing last digit of prod in it.
+….c) Update carry by storing remaining digits in carry.
+3) Put all digits of carry in res[] and increase res_size by number of digits in carry.""",
+    "Fibonacci":
+        """The Fibonacci numbers are the numbers in the following integer sequence.
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+Method 1 ( Use recursion )
+A simple method that is a direct recursive implementation.
+Time Complexity: T(n) = T(n-1) + T(n-2) which is exponential.
+
+Method 2 ( Use Dynamic Programming )
+We can avoid the repeated work done is the method 1 by storing the Fibonacci numbers calculated so far.
+
+Method 3 ( Space Optimized Method 2 )
+We can optimize the space used in method 2 by storing the previous two numbers only because that is all we need to get the next Fibonacci number in series.
+
+Time Complexity:O(n)
+Extra Space: O(1)
+""",
+    "Greatest_Common_Divisor":
+        """GCD (Greatest Common Divisor) or HCF (Highest Common Factor) of two numbers is the largest number that divides both of them. 
+
+A simple solution is to find all prime factors of both numbers, then find intersection of all factors present in both numbers. Finally return product of elements in the intersection.
+
+An efficient solution is to use Euclidean algorithm which is the main algorithm used for this purpose. The idea is, GCD of two numbers doesn’t change if smaller number is subtracted from a bigger number. 
+
+A more efficient solution is to use modulo operator in Euclidean algorithm.""",
+    "Lowest_Common_Multiple":
+        """LCM (Least Common Multiple) of two numbers is the smallest number which can be divided by both numbers.
+For example LCM of 15 and 20 is 60 and LCM of 5 and 7 is 35.
+
+A simple solution is to find all prime factors of both numbers, then find union of all factors present in both numbers. Finally return product of elements in union.
+
+
+An efficient solution is based on below formula for LCM of two numbers ‘a’ and ‘b’.
+
+   a x b = LCM(a, b) * GCD (a, b)
+
+   LCM(a, b) = (a x b) / GCD(a, b)""",
+    "Max_Pairwise_Product":
+        """Given an array with both +ive and -ive integers, return a pair with highest product.
+
+Examples :
+
+Input: arr[] = {1, 4, 3, 6, 7, 0}  
+Output: {6,7}  
+
+Input: arr[] = {-1, -3, -4, 2, 0, -5} 
+Output: {-4,-5}
+
+A Simple Solution is to consider every pair and keep track maximum product. Below is the implementation of this simple solution.
+
+Time Complexity : O(n2)
+
+A Better Solution is to use sorting. Below are detailed steps.
+1) Sort input array in increasing order.
+2) If all elements are positive, then return product of last two numbers.
+3) Else return maximum of products of first two and last two numbers.
+Time complexity of this solution is O(nLog n). Thanks to Rahul Jain for suggesting this method.
+
+An Efficient Solution can solve the above problem in single traversal of input array. The idea is to traverse the input array and keep track of following four values.
+a) Maximum positive value
+b) Second maximum positive value
+c) Maximum negative value i.e., a negative value with maximum absolute value
+d) Second maximum negative value.
+At the end of the loop, compare the products of first two and last two and print the maximum of two products. Below is the implementation of this idea.
+
+Time complexity: O(n)
+Auxiliary Space: O(1)
+""",
     "Prime_Numbers": """""",
     "Swap_Numbers": """""",
     "AVL_Tree": """""",
